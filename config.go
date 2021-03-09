3b149aefe00e354746208f8ec13a6af15c7a1e9b
David Hein <44842054+MasterZydra@users.noreply.github.com>
2021-03-09 20:36:36 +0000

    Add config for global constants

---
+++ config.go
@@ -0,0 +1,8 @@
+package godashing
+
+const (
+	// TestPath for files to use in tests
+	TestPath string = "../../test"
+	// TestOutputPath for files generated in tests
+	TestOutputPath string = "../../test/output"
+)
