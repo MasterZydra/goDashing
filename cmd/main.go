cfab38eba9231e4e5eb62a7664c856a65cd7d625
David Hein <44842054+MasterZydra@users.noreply.github.com>
2021-03-09 19:14:56 +0000

    Remove tokenAuthMiddleware for simplification

--- cmd/main.go
+++ cmd/main.go
@@ -1,51 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"crypto/subtle"
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
-	"goDashing"
+	dashing "goDashing"
 	_ "goDashing/jobs"
 )
-
-func tokenAuthMiddleware(h http.Handler) http.Handler {
-	auth := []byte(os.Getenv("TOKEN"))
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if len(auth) == 0 {
-			h.ServeHTTP(w, r)
-			return
-		}
-		if r.Method == "POST" {
-			body, _ := ioutil.ReadAll(r.Body)
-			r.Body.Close()
-			r.Body = ioutil.NopCloser(bytes.NewReader(body))
-
-			var data map[string]interface{}
-			json.Unmarshal(body, &data)
-			token, ok := data["auth_token"]
-			if !ok {
-				log.Printf("Auth token missing")
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-
-			if result := subtle.ConstantTimeCompare(auth, []byte(token.(string))); result != 1 {
-				log.Printf("Invalid auth token: expected %s, got %s", auth, token)
-				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-				return
-			}
-		}
-
-		h.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -64,8 +27,8 @@ func main() {
 	dash := dashing.NewDashing(webroot, port, os.Getenv("TOKEN")).Start()
 	log.Println("listening on :" + port)
 
-	// open.Run("http://127.0.0.1:" + port + "/")
+	http.Handle("/", dash)
 
-	log.Fatal(http.ListenAndServe(":"+port, tokenAuthMiddleware(dash)))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 
 }
