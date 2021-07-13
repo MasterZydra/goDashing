3655757b005f4d8db92463e30dc2874a4df78749
MasterZydra <david-hein196@web.de>
2021-07-13 21:49:53 +0000

    Add package packr and remove rice

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/GeertJohan/go.rice v1.0.2
 	github.com/PuerkitoBio/goquery v1.6.1
 	github.com/andygrunwald/go-jira v1.13.0
 	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
 	github.com/clbanning/mxj v1.8.4
+	github.com/gobuffalo/envy v1.9.0 // indirect
+	github.com/gobuffalo/packd v1.0.0 // indirect
+	github.com/gobuffalo/packr v1.30.1
 	github.com/husobee/vestigo v1.1.1
+	github.com/karlseguin/expect v1.0.8 // indirect
 	github.com/karlseguin/gerb v0.0.0-20150209104244-a6088cc65a6b // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/streamrail/concurrent-map v0.0.0-20160823150647-8bf1e9bacbf6
 	gopkg.in/fsnotify.v1 v1.4.7
 	gopkg.in/karlseguin/bytepool.v3 v3.0.4 // indirect
--- release.sh
+++ release.sh
@@ -9,12 +9,10 @@ mkdir ${releaseFolder} 2>/dev/null
 rm ${releaseFolder}*.tgz 2>/dev/null
 rm ${releaseFolder}*.zip 2>/dev/null
 
-rice embed-go
-
 arch=amd64
 os=darwin
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
@@ -22,7 +20,7 @@ mv $product.tgz ${releaseFolder}/
 arch=arm
 os=linux
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
@@ -30,7 +28,7 @@ mv $product.tgz ${releaseFolder}/
 arch=amd64
 os=linux
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
@@ -38,7 +36,7 @@ mv $product.tgz ${releaseFolder}/
 arch=386
 os=linux
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
@@ -46,7 +44,7 @@ mv $product.tgz ${releaseFolder}/
 arch=386
 os=windows
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
 zip -r ${product}.zip ${product}.exe
 rm $product.exe
 mv $product.zip ${releaseFolder}/
@@ -54,7 +52,7 @@ mv $product.zip ${releaseFolder}/
 arch=amd64
 os=windows
 product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
+env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
 zip -r ${product}.zip ${product}.exe
 rm $product.exe
 mv $product.zip ${releaseFolder}/
