829d5a273a4f4213c13b7b62f5b9667cadb54a15
MasterZydra <david-hein196@web.de>
2021-07-15 21:58:46 +0000

    Add comments Add debug output

--- cmd/godashing/assets.go
+++ cmd/godashing/assets.go
@@ -18,19 +18,28 @@ func ExtractAssets() {
 }
 
 func extractAsset(jobbox packr.Box) {
+	// Extract folder name from path
 	var assetPath = filepath.Base(jobbox.Path)
 
+	// Skip folder if it already exists
 	log.Println("Check for asset folder '" + assetPath + "'")
 	if ok, _ := utils.Exists(root + assetPath); ok {
 		return
 	}
 
+	// Extract every file in the box
 	log.Println("Extract asset folder '" + assetPath + "'")
 	jobbox.Walk(func(path string, f packr.File) error {		
+		// Make all folders
 		os.MkdirAll(root + assetPath + string(filepath.Separator) + filepath.Dir(path), 0777)
 
+		// Write data to files
 		content, _ := jobbox.FindString(path)
-		ioutil.WriteFile(root + assetPath + string(filepath.Separator) + path, []byte(content), 0644)
+		var filename = root + assetPath + string(filepath.Separator) + path
+		if (debugmode) {
+			log.Println("Extract asset file '" + filename + "'")
+		}
+		ioutil.WriteFile(filename, []byte(content), 0644)
 		return nil
 	})
 }
