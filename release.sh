a82703b2035b37e1d2fd16d19fb2445c96d55ee5
MasterZydra <david-hein196@web.de>
2021-07-22 21:00:41 +0000

    Remove version number from filename

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #https://gist.github.com/bclinkinbeard/1331790
-versionLabel=$1
 productName="goDashing"
 releaseFolder="release/"
 
@@ -11,7 +10,7 @@ rm ${releaseFolder}*.zip 2>/dev/null
 
 arch=amd64
 os=darwin
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
@@ -19,7 +18,7 @@ mv $product.tgz ${releaseFolder}/
 
 arch=arm
 os=linux
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
@@ -27,7 +26,7 @@ mv $product.tgz ${releaseFolder}/
 
 arch=amd64
 os=linux
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
@@ -35,7 +34,7 @@ mv $product.tgz ${releaseFolder}/
 
 arch=386
 os=linux
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product ./cmd/godashing/...
 tar czfv $product.tgz $product
 rm $product
@@ -43,7 +42,7 @@ mv $product.tgz ${releaseFolder}/
 
 arch=386
 os=windows
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
 zip -r ${product}.zip ${product}.exe
 rm $product.exe
@@ -51,7 +50,7 @@ mv $product.zip ${releaseFolder}/
 
 arch=amd64
 os=windows
-product=${productName}_${versionLabel}_${os}_${arch}
+product=${productName}_${os}_${arch}
 env GOOS=$os GOARCH=$arch packr build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe ./cmd/godashing/...
 zip -r ${product}.zip ${product}.exe
 rm $product.exe
