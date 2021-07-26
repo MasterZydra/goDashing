7fe796edc4054e89ed3b2caf423f503f1c7c18ab
MasterZydra <david-hein196@web.de>
2021-07-26 22:03:28 +0000

    Add Create func

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -1,6 +1,28 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Create the given file path. The path to the directory will be created
+// as well as the file itself.
+func Create(path string) error {
+	if strings.Contains(path, string(os.PathSeparator)) {
+		folderPath := filepath.Dir(path)
+		err := os.MkdirAll(folderPath, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+    f, err := os.Create(path)
+    if err != nil {
+        return err
+    }
+    f.Close()
+    return nil
+}
 
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
